ba9a4b61995af9b03a58e7bee59ad5e856c139f1
mmari_ya <bartoshmari@yandex.ru>
2021-03-27 21:05:55 +0700

    Добавлено новое приложение. Попробовала сделать шаблон для главной страницы (для пробы).

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+# Создаем новый класс. Например, Учителя
+# class Teacher(models.Model):
+# create new field
+# field = models.{field_type}('name_of_field', max_lenght, e.t.c.)
+# def __str__(self):
+#     return self.field, e.t.c.
+
+# python manage.py makemigrations
+# do migrations ->
+#     python manage.py migrate
+
+# python manage.py createsuperuser
+
+#   class Meta:
+#       verbose_mane = 'Учитель'
+#       verbose_name_plural = 'Учителя'
